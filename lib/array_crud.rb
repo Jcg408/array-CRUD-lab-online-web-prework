cba53fea6b3a344cd41f2a487f9d675c14a9cc7a
Jessica Gladstone <jcg321@comcast.net>
2018-09-27 14:23:28 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,10 +10,11 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array << element
+    array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
+    array.unshift(element)
   
 end
 
